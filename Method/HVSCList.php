1ddc565378ba2587972261854a335978cdcaf0cb
gizmore <gizmore@wechall.net>
2021-03-03 03:37:45 +0100

    6.10-r9506

--- Method/HVSCList.php
+++ Method/HVSCList.php
@@ -2,8 +2,13 @@
 namespace GDO\HVSC\Method;
 
 use GDO\Table\MethodQueryTable;
+use GDO\SID\GDO_SIDFile;
 
 final class HVSCList extends MethodQueryTable
 {
+    public function gdoTable()
+    {
+        return GDO_SIDFile::table();
+    }
     
 }
